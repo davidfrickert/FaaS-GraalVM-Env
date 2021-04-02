0109cf0d79afb3bbcf5db45f7bda3a92e9241c89
David Frickert <david.frickert@protonmail.com>
2021-04-02 12:56:30 +0100

    Initial

---
+++ Base-Function/src/main/java/pt/ulisboa/ist/func/BaseFunction.java
@@ -0,0 +1,7 @@
+package pt.ulisboa.ist.func;
+
+public class BaseFunction {
+	public String reverseString(String input) {
+		return new StringBuilder(input).reverse().toString();
+	}
+}
---
+++ Base-Function/src/test/java/pt/ulisboa/ist/func/BaseFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ulisboa.ist.func;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
