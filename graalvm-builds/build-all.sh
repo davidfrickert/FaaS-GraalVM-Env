22cde39afba2efd7aaac36761d2d2da989fe0c50
David Frickert <david.frickert@protonmail.com>
2021-10-15 23:00:29 +0100

    builds - new stuff

---
+++ graalvm-builds/build-all.sh
@@ -0,0 +1,4 @@
+./build.sh ../example\ 'functions/sleep' 'sleep'
+./build.sh ../example\ functions/filehashing filehashing
+./build.sh ../example\ functions/rest-api rest-api
+./build.sh ../example\ functions/video-processing video
--- graalvm-builds/build.sh
+++ graalvm-builds/build.sh
@@ -1,5 +1,15 @@
-docker-compose up
-cp "../FunctionRunner/target/app" openwhisk-runtime-nativeimage-basefunction/
+set -euxo pipefail
+
+if [ -z ${2+x} ]; then
+   echo "Script args: PATH_OF_FUNCTION FUNCTION_TAG [optional OUTPUT_IMAGE]"
+fi
+
+DOCKER_IMAGE="${3:-'openwhisk-runtime-nativeimage-basefunction'}"
+
+cp -r mvnw/mvnw "$1"
+cp -r mvnw/.mvn "$1"
+FUNCTION="$1" docker-compose up
+cp "$1/target/app" openwhisk-runtime-nativeimage-basefunction/
 cd openwhisk-runtime-nativeimage-basefunction
-docker build . -t "davidfrickert/openwhisk-runtime-nativeimage-basefunction:sleep"
-docker push "davidfrickert/openwhisk-runtime-nativeimage-basefunction:sleep"
+docker build . -t "davidfrickert/$DOCKER_IMAGE:$2"
+docker push "davidfrickert/$DOCKER_IMAGE:$2"
